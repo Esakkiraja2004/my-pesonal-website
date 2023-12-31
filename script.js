0a5c717059dfe163445461dd35ad7d86c9d2755c
Esakkiraja2004 <esakkiraja2401@gmail.com>
2023-12-31 22:39:46 +0530

    file modified

--- script.js
+++ script.js
@@ -1,4 +1,26 @@
+// code for Resume button
 
+var resumebtn = document.querySelector("#resume-btn");
+
+var resume = resumebtn.addEventListener("click", () => {
+    var pdfUrl = 'Esakki Raja - Resume.pdf.pdf';
+
+    // Open the PDF in a new window or tab
+    window.open(pdfUrl, '_blank');
+});
+
+// code forPeoject button
+var projectbtn = document.querySelector('#project-btn');
+
+var project = projectbtn.addEventListener('click', ()=>{
+        var url = 'https://github.com/Esakkiraja2004?tab=repositories';
+    
+        // Open the URL in a new window or tab
+        window.open(url, '_blank');
+});
+
+
+// code for Responsive
 
 window.addEventListener('load', function() {
     var isMobile = window.matchMedia("only screen and (max-width: 767px)").matches;
