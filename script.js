7910b1c3300f7d989924087e75e99dd406bdb970
Esakkiraja2004 <esakkiraja2401@gmail.com>
2024-01-16 16:09:05 +0530

    FM

--- script.js
+++ script.js
@@ -66,6 +66,35 @@ window.addEventListener('load', function() {
 
     if (isMobile) {
         body.innerHTML += existingElement.outerHTML;
+        // code for Resume button
+
+var resumebtn = document.querySelector("#resume-btn");
+
+var resume = resumebtn.addEventListener("click", () => {
+    var pdfUrl = 'Esakki Raja - Resume.pdf.pdf';
+
+    // Open the PDF in a new window or tab
+    window.open(pdfUrl, '_blank');
+});
+
+// code forPeoject button
+var projectbtn = document.querySelector('#project-btn');
+
+var project = projectbtn.addEventListener('click', ()=>{
+        var url = 'https://github.com/Esakkiraja2004?tab=repositories';
+    
+        // Open the URL in a new window or tab
+        window.open(url, '_blank');
+});
+
+// code for contact btn
+
+const contactbtn = document.querySelector('#contact-btn');
+
+var contact = contactbtn.addEventListener('click',() => {
+    window.location.href = 'contact.html';
+});
+
         
         
     } else {
